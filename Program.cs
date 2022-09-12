90c39bdcd24c1159cda6394e0904d5c6328a45c0
sarahebrooks12 <sarahebrooks12@gmail.com>
2022-09-12 13:53:56 -0400

    phase 2 complete

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace moose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
