441dd9b9cb2eda5d36be6ab4ddc564d33f8c4727
sarahebrooks12 <sarahebrooks12@gmail.com>
2022-09-12 13:55:06 -0400

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 
-namespace moose
+namespace EnthusiasticMoose
 {
     class Program
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine();
+
+            //Call method to get Mr. Moose to show up
+            MooseSays();
+        }
+
+        static void MooseSays()
+        {
+            Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
         }
     }
 }
